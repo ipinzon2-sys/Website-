b5625bbcfea02a76405dee7217845eb6194188f9
ipinzon2-sys <ipinzon2@udi.edu.co>
2026-01-19 21:41:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Selecciona el div con id "card-bo"
+  const cardBO = document.getElementById("card-bo");
+
+  // Agrega un evento de clic
+  cardBO.addEventListener("click", () => {
+    // Redirige al archivo HTML deseado
+    window.location.href = "bo.html";
+  });
+});
 
